c183e6208b75166badc19ae9cf57dbb26568a491
khatab_majzob <katab.abden8@gmail.com>
2024-05-14 22:03:41 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Puppet script for creating a file
-
-file { '/tmp/school':
-  ensure  => present,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,4 +0,0 @@
-# Kill a process name "killmenow". Poor thing..
-exec { 'killmenow':
-  command => '/usr/bin/pkill -f killmenow',
-}
