44d32676a66c1a94c52c687dfa2eead1765131d2
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 <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>
2024-05-29 17:39:23 -0700

    add wekzeug 2.2.2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 ##!/usr/bin/pup
 # Using Puppt to install flask from pip3
+package {'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3'
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
