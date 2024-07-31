3347e81a9e94f5c8de566cf32726016b935c1ef0
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 <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>
2024-07-31 13:33:57 -0700

    json place holder

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
-"""This module scraps REST API from a placeholder website"""
+"""
+A Script that, uses this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+"""
+
+import json
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    if len(argv) < 2:
-        exit()
-    response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                            .format(argv[1])).json()
-    print("Employee {} is done with tasks"
-          .format(response.get('name')), end='')
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
-    response = requests.get(url).json()
-    task_list = []
-    nb_tasks = len(response)
-    nb_tasks_completed = 0
-    for task in response:
-        if task.get('completed') is True:
-            nb_tasks_completed += 1
-            task_list.append(task.get('title'))
-    print('({}/{}):'.format(nb_tasks_completed, nb_tasks))
-    for task in task_list:
-        print("\t " + task)
+
+    sessionReq = requests.Session()
+
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    name = employeeName.json()['name']
+
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, totalTasks, len(json_req)))
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            print("\t " + done_tasks.get('title'))
