350c4031ee3831e2e886ad9f68c3560ce960a907
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 <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>
2024-08-09 07:38:42 -0700

    fix empluee name

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,34 +4,23 @@ A Script that, uses this REST API, for a given employee ID, returns
 information about his/her TODO list progress
 """
 
-import json
 import requests
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
 
-    sessionReq = requests.Session()
-
-    idEmp = argv[1]
-    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
-    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
-
-    employee = sessionReq.get(idURL)
-    employeeName = sessionReq.get(nameURL)
-
-    json_req = employee.json()
-    name = employeeName.json()['name']
-
-    totalTasks = 0
-
-    for done_tasks in json_req:
-        if done_tasks['completed']:
-            totalTasks += 1
-
-    print("Employee {} is done with tasks({}/{}):".
-          format(name, totalTasks, len(json_req)))
-
-    for done_tasks in json_req:
-        if done_tasks['completed']:
-            print("\t " + done_tasks.get('title'))
+    emp_id = sys.argv[1]
+    user_res = requests.get(url + "users/{}".format(emp_id))
+    user = user_res.json()
+    params = {"userId": emp_id}
+    todos_res = requests.get(url + "todos", params=params)
+    todos = todos_res.json()
+    completed = []
+    for todo in todos:
+        if todo.get("completed") is True:
+            completed.append(todo.get("title"))
+    print("Employee {} is done with tasks({}/{})".format(user.get("name"),
+                                                         len(completed), len(todos)))
+    for complete in completed:
+        print("\t {}".format(complete))
